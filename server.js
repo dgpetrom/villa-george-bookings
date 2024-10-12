27bb0b879f6318c57de70161fd29704cb245de86
dgpetrom <dgpetrom@gmail.com>
2024-10-12 12:31:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Enable CORS and body parsing
 app.use(cors());
